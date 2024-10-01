d8f5612a7e616c29ec173d94c7da7e5742962375
noorul <noorul.khan@zettalabs.com>
2024-10-01 17:09:48 +0530

    Keycloak oauth implemented: now to access route we needt to pass token generated from keycloak postman

--- keycloakIntegrationInSb/src/main/java/com/example/keycloakIntegrationInSb/controller/HomeControlller.java
+++ keycloakIntegrationInSb/src/main/java/com/example/keycloakIntegrationInSb/controller/HomeControlller.java
@@ -9,20 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/kc/v1")
 public class HomeControlller {
 
+    @GetMapping("/")
+    public ResponseEntity<String> myHomeBasePage(){
+        return ResponseEntity.ok("This is my home Base page /");
+    }
+
     @GetMapping("/home")
     public ResponseEntity<String> myHomePage(){
-        return ResponseEntity.ok("This is my home page");
+        return ResponseEntity.ok("This is my home page /home");
     }
 
 
     @GetMapping("/home2")
     public ResponseEntity<String> myHomePage2(){
-        return ResponseEntity.ok("Hello From Spring boot & keycloak");
+        return ResponseEntity.ok("Hello From Spring boot & keycloak /home2");
     }
 
 
     @GetMapping("/home3")
     public ResponseEntity<String> myHomePage3(){
-        return ResponseEntity.ok("Hello From Spring boot & keycloak - ADMIN");
+        return ResponseEntity.ok("Hello From Spring boot & keycloak - ADMIN   /home3");
     }
 }
